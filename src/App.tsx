0299aabf40226fe1b5e23309bce0b7721a6f81f7
Nicolás Londoño <nicolas.londono@veevart.com>
2022-10-15 16:06:16 -0500

    Add: eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "src/App.css";
+import 'src/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Ask People</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Ask People</h1>
+		</div>
+	);
 }
 
 export default App;
